7266cf12c6ebe1a0b72c52e27093cb8b814e4e4e
KennedyParks <kennedyjparks@gmail.com>
2022-10-11 16:36:47 -0500

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Kennedy Parks on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
